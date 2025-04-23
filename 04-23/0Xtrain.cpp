1b0ac6ceebb35995bbfea796643e175917c0acf4
BCXNSD <77572974+BCXNSD@users.noreply.github.com>
2025-04-23 15:41:45 +0800

    Update 0Xtrain.cpp

--- 04-23/0Xtrain.cpp
+++ 04-23/0Xtrain.cpp
@@ -1,1 +1,19 @@
+/*
+https://www.luogu.com.cn/problem/P1966
 
+输入 n(1≤n≤1e5) 和两个长为 n 的数组 a 和 b，元素范围 [0,2^31)。保证 a 中没有重复元素，b 中没有重复元素。
+
+每次操作，你可以交换 a 中的一对相邻元素，或者 b 中的一对相邻元素。
+为了最小化 (a[i]-b[i])^2 之和，至少要操作多少次？
+输出【最小操作次数】模 1e8 - 3 的结果。
+
+离散化+排序+置换+逆序对
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+  return 0;
+}
